9adbb706c510703631cf352136c849b4e8c46a85
Long <64343005+broripeu19@users.noreply.github.com>
2020-05-21 13:32:33 +0700

    Add files via upload

---
+++ bai1.js
@@ -0,0 +1,31 @@
+function no1() {
+	document.getElementById('no1').style.display = 'none';
+	document.getElementById('no2').style.display = 'block';
+}
+function no2() {
+	document.getElementById('no2').style.display = 'none';
+	document.getElementById('no3').style.display = 'block';
+}
+function no3() {
+	document.getElementById('no3').style.display = 'none';
+	document.getElementById('no4').style.display = 'block';
+}
+function no4() {
+	document.getElementById('no4').style.display = 'none';
+	document.getElementById('no5').style.display = 'block';
+}
+function no5() {
+	document.getElementById('no5').style.display = 'none';
+	document.getElementById('no6').style.display = 'block';
+}
+function no6() {
+	document.getElementById('no6').style.display = 'none';
+	document.getElementById('end').style.display = 'block';
+}
+function end() {
+	alert("Đừng chầy cối nữa em à :v");
+	document.getElementById('end').style.display= 'none';
+}
+function yes() {
+	alert("Chúc mừng em đã về đội của anh :v :v :v");
+}
